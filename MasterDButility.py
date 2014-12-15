1d9086e427dbff7b3fb8a3e7c327f57312f1fa4c
njancsar <njancsar@gmail.com>
2014-12-14 21:59:56 -0500

    t

--- MasterDButility.py
+++ MasterDButility.py
@@ -1,1 +1,10 @@
-import sqlite3
+import MasterParse as mp
+import os
+import sys
+import sqlalchemy
+
+# Standalone Script Section
+if __name__ == "__main__":
+    fname = r'C:\Users\CJANCSAR\Documents\FNBUG\ALL.DAT'
+    ret = mp.parseMaster(fname)
+ 
